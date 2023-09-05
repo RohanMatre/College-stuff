a166df1a58abb41b17818502289a08b345675d7e
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-09-05 10:49:41 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,21 @@
-from exception import *
+# import pywhatkit as kit
+# import pyautogui as pg
 
-with open('requirement.txt', 'r') as f:
+# kit.sendwhatmsg('+91 99989 28777','Systummm'*2000,16,27,15)
 
-    lines = [line.strip() for line in f.readlines()]
-
-    info = {k: v for line in lines[:3]
-            if ':' in line for k, v in [line.split(':')]}
-
-    if info.get('OS') == None or info['OS'] == '':
-        raise Os_Not_found('OS not found')
-
-    if info['OS'].lower() != 'ubuntu':
-        raise Os_not_ubuntu('OS is not Ubuntu')
-
-    if info.get('OS_Version') == None or info['OS_Version'] == '':
-        raise Os_version_not_found('OS Version not found')
-
-    if info.get('Python') == None or info['OS'] == '':
-        raise Python_version_not_found('Python version has not been mentioned')
-
-    if len(info) == len(lines):
-        raise Library_not_found('No libraries found in the requirements file')
-
-    libraries = {}
-    for line in lines[len(info):]:
-        if '==' in line:
-            k, v = line.split('==')
-            libraries[k] = v
-        elif line != '':
-            raise OLibrary_version_not_found(f"No version mentioned for {line}")
-
-    if len(libraries) == 0:
-        raise Library_not_found('No libraries found in the requirements file')
-
-with open('Dockerfile', 'w') as f:
-    f.write(f"FROM {info['OS'].lower()}:{info['OS_Version']}\n\n")
-    f.write(f"RUN apt-get install python{info['Python']}\n\n")
-
-    for library in libraries:
-        f.write(
-            f"RUN python3 -m pip install {library}=={libraries[library]}\n")
-
-    f.write(f'\nRUN echo "Compilation Successful"\n')
+import pywhatkit as kit
+import datetime
+import pyautogui as pg
+import time
+number = '+91 90810 75280'
+msg = 'hello'
+hour = 9
+min = 29
+kit.sendwhatmsg(number, msg, hour, min, 15)
+# time.sleep(2)
+message = "Systummm"
+for i in range(1000):
+    pg.typewrite(message)
+    time.sleep(0.0000001)
+    pg.press('enter')
+    
