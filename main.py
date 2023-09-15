65f127e596111cca28325a203adbf48d33c51c4c
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-09-15 23:07:13 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-import time
-name = input('Enter your name: ')
-recenttime = time.strftime('%H:%M:%S')
-Recenttime = int(time.strftime('%H'))
-c= name.capitalize()
-if(4<=Recenttime<12):
-    print('GOOD MORNING',c,'\nits',recenttime)
-elif(12>=Recenttime<17):
-    print('GOOD EVENING',c,'\nits',recenttime)
-else:
-    print('GOOD NIGHT',c,'\nits',recenttime)
-# print('Hii',name,'its',recenttime)
-# Morning Time : 04:00:00 to 11:59:59
-# Afternoon Time : 12:00:00 to 16:59:59
-# Evening Time : 17:00:00 to 20:59:59
-# Night Time : 21:00:00 to 03:59:59
+import shutil
+
+# read mode 
+# with open('data.txt','r') as f:
+#     print(f.read())
+
+# append
+# with open('data.txt','a') as f:
+#     f.write('Hello')
+
+# shutil.copy('data.txt','destination')
+
+shutil.move('sample.txt','destination')
