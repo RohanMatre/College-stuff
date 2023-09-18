94eee5f9a714596874cfc1bc07c796a0fa478c2c
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-09-18 14:15:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,74 +1,16 @@
-import csv
-import pandas as pd
-import pyautogui
 import time
-import os
-
-# --- Step 1: Read text file and Create CSV ---
-txt_filename = './data.txt'
-csv_filename = './output.csv'
-
-# Read text file
-data = []
-with open(txt_filename, 'r') as file:
-    lines = file.readlines()
-    for line in lines[1:]:
-        row = line.strip().split(',')
-        data.append(row)
-
-# Create CSV file
-with open(csv_filename, 'w', newline='') as csvfile:
-    csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(["No", "Customer Name", "Customer Id", "Food Id", "Food Name", "Food Review"])
-    csv_writer.writerows(data)
-
-# --- Step 2: Data Analysis ---
-df = pd.read_csv(csv_filename)
-
-# Simple Analysis: Count of each food item
-food_count = df['Food Name'].value_counts()
-
-# Additional Analyses
-summary_stats = df['No'].describe()
-unique_customers = df['Customer Id'].nunique()
-common_words = pd.Series(' '.join(df['Food Review'].astype(str)).lower().split()).value_counts()[:10]
-food_per_customer = df.groupby('Customer Name')['Food Name'].count()
-
-# Combine all analyses into one string
-analysis_str = f"""--- Food Count ---
-{food_count}
-
---- Additional Analysis ---
-Summary Stats for 'No':
-{summary_stats}
-
-Unique Customers: {unique_customers}
-
-Common Words in Food Reviews: {common_words}
-
-Food Items per Customer:
-{food_per_customer}
-"""
-
-time.sleep(2)
-# --- Step 3: Open TextEdit and Pasting Analysis ---
-os.system("open /Applications/Microsoft\ Word.app")
-time.sleep(3)
-pyautogui.hotkey('command', 'o')  
-time.sleep(2)
-pyautogui.press('enter')
-time.sleep(1)
-
-# Paste the analysis
-pyautogui.write(analysis_str)
-time.sleep(2)
-
-# --- Step 4: Save TextEdit File ---
-pyautogui.hotkey('command', 's')  # Save the file
-time.sleep(2)
-pyautogui.write('./analysis_report')
-pyautogui.press('enter')
-time.sleep(2)
-
-# Close TextEdit
-pyautogui.hotkey('command', 'q')
+name = input('Enter your name: ')
+recenttime = time.strftime('%H:%M:%S')
+Recenttime = int(time.strftime('%H'))
+c= name.capitalize()
+if(4<=Recenttime<12):
+    print('GOOD MORNING',c,'\nits',recenttime)
+elif(12>=Recenttime<17):
+    print('GOOD EVENING',c,'\nits',recenttime)
+else:
+    print('GOOD NIGHT',c,'\nits',recenttime)
+# print('Hii',name,'its',recenttime)
+# Morning Time : 04:00:00 to 11:59:59
+# Afternoon Time : 12:00:00 to 16:59:59
+# Evening Time : 17:00:00 to 20:59:59
+# Night Time : 21:00:00 to 03:59:59
